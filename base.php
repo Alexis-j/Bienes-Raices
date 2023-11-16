4c4ec37ce2bdf75b7931e75a305610d15cad996c
Tu Nombre <alexis.jcastillo@outlook.com>
2023-11-16 11:08:48 +0100

    adding php

---
+++ base.php
@@ -0,0 +1,20 @@
+<?php
+    include'./includes/templates/header.php';
+?>
+    <main class="contenedor seccion">
+        <h1>Base</h1>
+    </main>
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </nav>
+        </div>
+        <p class="copyright">Todos os Derechos Reservados 2023©</p>
+    </footer>
+    <script src="build/js/bundle.min.js"></script>
+</body>
+</html>
